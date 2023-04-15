20919ed8c2b5931c88a542d6b010a7c7d47e1299
MatiasCornalo <130251779+MatiasCornalo@users.noreply.github.com>
2023-04-15 10:19:16 -0300

    Update holamundo.c

--- holamundo.c
+++ holamundo.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int main(){
-    printf("Hello World");
-    return 0;
+    FILE *archivo;
+    archivo = fopen("output.txt","w");
+    fprintf(archivo,"Hello World\n");
+    fclose(archivo);
 }
